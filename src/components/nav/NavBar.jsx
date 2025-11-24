e9e41bf2818fc80e43f7e28458e4050d53f833ee
Rafay Fazal <rafaymx@gmail.com>
2025-11-24 17:35:00 -0600

    Added logo to navbar

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
 export default function NavBar() {
   return (
     <header className="w-full px-8 py-6 flex justify-center items-center z-20 relative">
+      {" "}
       <h1 className="text-2xl font-audiowide text-white tracking-widest">
-        CODEPACT
-      </h1>
+        {" "}
+        CODEPACT{" "}
+      </h1>{" "}
     </header>
   );
 }
